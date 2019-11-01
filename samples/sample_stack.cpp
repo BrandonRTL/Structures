7005419892632ae195afd60df8f86164248566cb
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2019-11-01 16:58:44 +0300

    Оператор присваивания, тесты

--- samples/sample_stack.cpp
+++ samples/sample_stack.cpp
@@ -4,21 +4,13 @@
 using namespace std;
 int main()
 {
-	/*TMinStack<int> A;
+	TMinStack<int> A;
 	A.Push(2);
 	A.Push(10);
 	A.Push(1);
 	A.Push(-10);
 	int x = A.FindMin();
-	cout << x; */
-	double x;
-	string A = "2+e(e(1)/e(1)-1)";
-	bool a = ExpressionCheck(A);
-	cout  << a << endl;
-	string B = ExpressionParse(A);
-	cout << B;
-	x = ExpressionSolveV2(B);
-	cout << endl << x;
+	cout << x; 
 	cin >> x;
 	return 0;
 }
