bdc8d41932a070f12532a0645e41892c41fb4fea
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2020-04-17 19:30:18 +0300

    Letter that you rip-rip

--- include/UHeap.h
+++ include/UHeap.h
@@ -27,11 +27,90 @@ template <typename ValType>
 class heap
 {
 public:
-	std::vector<priority_elem<ValType>> ar;
+	std::vector<priority_elem<ValType>> data;
 	int demension;
-	heap(int _d)
+	heap(int _d = 2)
 	{
-		demention = _d;
+		demension = _d;
+	}
+	int approachMyChild(int id, int childInd)
+	{
+		if (childInd >= demension || childInd < 0)
+			throw "WTF!?!?!";
+		return (demension*id + childInd);
+	}
+	int parent(int id)
+	{
+		return ((id - 1) / demension);
+	}
+	int minChild(int id)
+	{
+		int min = approachMyChild(id; 0);
+		for (int i = 0; i < demension; i++)
+		{
+			if (data[approachMyChild(id), i] < data[min])
+			{
+				min = i;
+			}
+		}
+		return min;
+	}
+	void swap(int id1, int id2)
+	{
+		priority_elem<ValType> tmp = data[j1];
+		data[j1] = data[j2];
+		data[j2] = tmp;
+	}
+	void diving(int id)
+	{
+		int j1, j2;
+		j1 = id;
+		j2 = minChild(id);
+		while (j2 != -1 && data[j1] > data[j2])
+		{
+			data.swap(j1, j2);
+			//swap(j1, j2);
+			j1 = j2;
+			j2 = minChild(j1);
+		}
+	}
+	void emersion(int id)
+	{
+		int j1, j2;
+		j1 = id;
+		j2 = parent(id);
+		while (data[j1] < data[j2] && j2!= -1)
+		{
+			data.swap(j1, j2);
+			j1 = j2;
+			j2 = parent(j1);
+		}
+	}
+	void insert(priority_elem<ValType> el)
+	{
+		data.push_back(el);
+		emersion(data.size() - 1);
+	}
+	void deleteMin()
+	{
+		data.swap(0, data.size() - 1);
+		data.pop_back();
+		diving(0);
+	}
+	void decreaseWeight(int id, int delta)
+	{
+		data[id].priority -= delta;
+		emersion(id);
+	}
+	void Delete(int id)
+	{
+		decreaseWeight(id, INF);
+		deleteMin();
+	}
+	void makeHeap()
+	{
+		for (int i = data.size() - 1; i >= 1; i++)
+			diving(i);
 	}
 };
 #endif
--- samples/sample_heap.cpp
+++ samples/sample_heap.cpp
@@ -4,6 +4,14 @@ void main()
 {
 	priority_elem<int> A(1, 3);
 	priority_elem<int> B(2, 3);
-	std::cout << (A>B);
-	std::cout << (A<B);
+	//std::cout << (A>B);
+	//std::cout << (A<B);
+	heap<int> H;
+	std::cout << H.approachMyChild(0, 0) << std::endl;
+	std::cout << H.approachMyChild(0, 1) << std::endl;
+	std::cout << H.approachMyChild(1, 0) << std::endl;
+	std::cout << H.approachMyChild(1, 1) << std::endl;
+	std::cout << H.parent(1) << std::endl;
+	std::cout << H.parent(3) << std::endl;
+
 }
